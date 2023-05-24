cb1508d2cc7dc92f74c40edd6b60b4c59519be28
dhineshwaran2595 <dhineshwaran2595@gmail.com>
2023-05-24 12:49:56 +0530

    Deployed website

---
+++ src/components/Signup.js
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+import firebase from "firebase"
+import { auth,createUserDocument } from "../firebase"
+
+export default function Signup(props) {
+ 
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const { signup } = useAuth()
+  const [error, setError] = useState("")
+  const [message, setMessage] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+  const { errors } = useAuth()
+  console.log("pp",errors)
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match")
+    }
+
+      firebase.auth().createUserWithEmailAndPassword(emailRef.current.value, passwordRef.current.value)
+     .then((userCredential) => {
+       // User creation successful
+       const user = userCredential.user;
+       console.log("User created:", user);
+       createUserDocument(user);
+       setError('')
+       firebase.auth().signOut();
+       setMessage("Please wait till your accout is approved")
+     })
+     .catch((error) => {
+       setError(error.message);
+       // User creation failed
+       const errorCode = error.code;
+       const errorMessage = error.message;
+       console.error("Error creating user:", errorCode, errorMessage);
+     });
+       
+
+    setLoading(false)
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="success">{message}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? <Link to="/login">Log In</Link>
+      </div>
+    </>
+  )
+}
